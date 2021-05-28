6e3705ab1871e85543e6f3c373064957f68c66d8
gnwncpta <gnwncpta@gmail.com>
2021-05-28 20:35:43 +0700

    Working on calculateAll process

--- src/Calculate/CalculateAll.js
+++ src/Calculate/CalculateAll.js
@@ -1,31 +1,63 @@
 import { log } from '../Fazt/Fazt';
 
-const calculateAll = (arr) => {
-    let angka = [];
-    let operator = [];
-    let calc = "";
+// const calculateAll = (arr) => {
+//     let angka = [];
+//     let operator = [];
+//     let calc = ""; // berisi 
 
-    // memasukann angka dan operator ke array terpisah;
-    arr.forEach(isi => {
-        if(typeof isi === "number"){
-            angka.push(isi);
-        } else if(typeof isi === "string"){
-            operator.push(isi);
-        }
+//     // memasukann angka dan operator ke array terpisah;
+//     arr.forEach(isi => {
+//         if(typeof isi === "number"){
+//             angka.push(isi);
+//         } else if(typeof isi === "string"){
+//             operator.push(isi);
+//         }
+//     });
+
+//     for(let i = 0; i < arr.length; i++){
+
+//         if(typeof arr[i] === "number"){
+//             calc += arr[i]
+//         } else if(typeof arr[i] === "string"){
+//             calc += arr[i]
+//         }
+
+//     };
+
+//     // let removeComma = calc.join(' ');
+//     // let arrange = removeComma.split(' ');
+//     // log(arrange);
+//     // log(calc.slice(0, 5));
+
+//     for(let i = 0; i < calc.length; i++){
+//         if(calc[i] === '+'){
+//             log('Ada +');
+//         }
+//     }
+// }
+
+const calculateAll = (numbers) => {
+
+    let str = numbers.toString(); // <- jadikan string
+    let removeComa = str.split(","); // <- jadikan array
+    let merge = removeComa.reduce((acc, val) => {
+        return acc + val; // <- gabungkan menjadi string
     });
+    log(merge)
+    log(merge.split('+')); // <- pisah angkanya
+    
+    let operator = [];
+    // if(merge.split('+')){
 
-    for(let i = 0; i < arr.length; i++){
-        if(typeof arr[i] === "number"){
-            calc += arr[i]
-        } else if(typeof arr[i] === "string"){
-            calc += arr[i]
-        }
-    };
+    // }
 
-    // let removeComma = calc.join(' ');
-    // let arrange = removeComma.split(' ');
-    // log(arrange);
-    log(calc.includes('+'));
+    // for(let i = 0; i < str.length; i++){
+    //     if(str[i] == '+'){
+    //         angkas.push(parseInt(str.slice(0, i)));
+    //     } 
+    // }
+
+    // log(angkas);
 }
 
 export default calculateAll;
--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -3,35 +3,48 @@ import { $, log } from '../Fazt/Fazt';
 import ToggleDark from '../DarkMode/DarkMode';
 import calculateAll from '../Calculate/CalculateAll';
 
+// tempat untuk menyimpan angka dan operator
 let numbers = [];
 
+// DOM Selection
 const buttons = $.querySelectorAll('.button-grid button');
 const p = $.querySelector('.display p');
 const clear = $.querySelector('.clear');
 const mode = $.querySelector('.mode');
 
+// jika localStorage memiliki mode dark maka atur menjadi tampilan dark mode
 if(localStorage.getItem('mode') == 'dark'){
     ToggleDark();
 }
 
+// jika yang di klik adalah tombol toggle dark mode
 mode.addEventListener('click', () => {
     ToggleDark();
 });
 
+// Looping semua button untuk nantinya di select
 buttons.forEach(button => {
+
+    // jika ada button yang di select 
     button.addEventListener('click', (e) => {
         
+        // Jika yang diklik adalah sama dengan maka kalkulasi
         if(e.target.classList.contains('equals')){
             calculateAll(numbers);
+        
+        // tapi selain tombol = yang di klik 
         } else {
-            let number = e.target.textContent;
-            p.textContent += number;
 
-            // push to numbers array
-            if(number === "+"){
+            // tampilkan ke DOM
+            let number = e.target.textContent; // <- berisi angka, operator
+            p.textContent += number; // <- tampilkan ke DOM
+
+            // dan setor ke Array nilainya untuk di kalkulasi
+            // jika yang di klik adalah operator expression push
+            if(number === "+" || number === "-" || number === "x" || number === "/"){
                 numbers.push(number);
             } else {
-                numbers.push(parseInt(number));
+                numbers.push(parseInt(number)); // masukkan nilai ke array menjadi tipe number
             }
 
         }
